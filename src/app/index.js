3286258880fb4068fcbcdce787b28f7742be7a0b
Magkinney21 <magkinney21@gmail.com>
2017-06-21 19:49:01 -0400

    added css and split into two components

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+require('./css/index.css');
+//Module require
+var TodoItem = require('./todoItem');
 
 //Create Component
 var TodoComponent = React.createClass({
@@ -33,24 +36,6 @@ var TodoComponent = React.createClass({
     this.setState({
       todos: updatedTodos
     });
-  }
-});
-
-//create todoltem component
-var TodoItem = React.createClass({
-  render: function(){
-    return(
-      <li>
-        <div className="todo-item">
-        <span className="item-name">{this.props.item}</span>
-        <span className="item-delete" onClick={this.handleDelete}> x </span>
-        </div>
-      </li>
-    );
-  },
-  //Custom functions
-  handleDelete: function(){
-    this.props.onDelete(this.props.item);
   }
 });
 
---
+++ src/app/todoItem.js
@@ -0,0 +1,23 @@
+//create todoltem component
+var React = require('react');
+require('./css/todoItem.css');
+//create TodoItem component
+var TodoItem = React.createClass({
+  render: function(){
+    return(
+      <li>
+        <div className="todo-item">
+        <span className="item-name">{this.props.item}</span>
+        <span className="item-delete" onClick={this.handleDelete}> x </span>
+        </div>
+      </li>
+    );
+  },
+  //Custom functions
+  handleDelete: function(){
+    this.props.onDelete(this.props.item);
+  }
+});
+
+
+module.exports = TodoItem;
