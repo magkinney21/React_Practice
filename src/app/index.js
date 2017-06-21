cedbacb793c01175fb84911643cfddc6a988ffce
Magkinney21 <magkinney21@gmail.com>
2017-06-21 12:54:55 -0400

    first props

--- src/app/index.js
+++ src/app/index.js
@@ -5,9 +5,13 @@ var ReactDOM = require('react-dom');
 var TodoComponent = React.createClass({
   render: function(){
     return(
-           <h1>Helllllo</h1>
+           <div>
+            <p><strong>Cheese name:</strong> {this.props.cheese.name}</p>
+            <p><strong>Cheese smellFactor:</strong> {this.props.cheese.smellFactor}</p>
+            <p><strong>Cheese price:</strong> {this.props.cheese.price}</p>
+           </div>
    );
   }
 });
-
-ReactDOM.render(<TodoComponent/>, document.getElementById('todo-wrapper'));
+var myCheese ={name:"Brie", smellFactor: "Stinky", price:"3.5"};
+ReactDOM.render(<TodoComponent mssg="I like cheese" cheese={myCheese}/>, document.getElementById('todo-wrapper'));
