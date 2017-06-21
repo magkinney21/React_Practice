859575831245327794451cba380f8f381ecca15b
Magkinney21 <magkinney21@gmail.com>
2017-06-21 12:22:30 -0400

    added first component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,13 @@
-alert('it works');
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+//Create Component
+var TodoComponent = React.createClass({
+  render: function(){
+    return(
+           <h1>Helllllo</h1>
+   );
+  }
+});
+
+ReactDOM.render(<TodoComponent/>, document.getElementById('todo-wrapper'));
